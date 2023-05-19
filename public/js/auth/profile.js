7c9602c5e4ec3040acc7bbeb5dd9e647178ec8a0
King-Julian-Pool <78912385+King-Julian-Pool@users.noreply.github.com>
2023-05-19 15:19:34 +0200

    organisation js

--- public/js/auth/profile.js
+++ public/js/auth/profile.js
@@ -1,47 +1,59 @@
-const map = document.querySelector("#map-container");
-
+//#region déclarations
 var mymap = L.map("map").setView([0, 0], 13);
 var latitudeInput = document.getElementById("latitude");
 var longitudeInput = document.getElementById("longitude");
-
 var marker;
 var latlng;
+//#endregion
 
-if (latitudeInput && longitudeInput) {
-    console.log(latitudeInput.value);
-    console.log(longitudeInput.value);
-    latlng = L.latLng(latitudeInput.value, longitudeInput.value);
+//#region événements
+window.addEventListener("DOMContentLoaded", (event) => {
+    setLocation();
+    initMap();
+});
+//#region
 
+//#region fonctions
+function setLocation() {
+    if (latitudeInput && longitudeInput) {
+        console.log(latitudeInput.value);
+        console.log(longitudeInput.value);
+        latlng = L.latLng(latitudeInput.value, longitudeInput.value);
+
+        mymap.setView(latlng, 13);
+        if (marker) {
+            marker.setLatLng(latlng);
+        } else {
+            marker = L.marker(latlng).addTo(mymap);
+        }
+    }
+
+    if (latlng == null) {
+        var lat = 49.3823959;
+        var lng = 1.0751564;
+        latlng = L.latLng(lat, lng);
+        latitudeInput.value = lat;
+        longitudeInput.value = lng;
+    }
+}
+
+function initMap() {
     mymap.setView(latlng, 13);
     if (marker) {
         marker.setLatLng(latlng);
     } else {
         marker = L.marker(latlng).addTo(mymap);
     }
-}
 
-if (latlng == null) {
-    var lat = 49.3823959;
-    var lng = 1.0751564;
-    latlng = L.latLng(lat, lng);
-    latitudeInput.value = lat;
-    longitudeInput.value = lng;
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+            '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+        tileSize: 512,
+        zoomOffset: -1,
+    }).addTo(mymap);
+    mymap.on("click", onMapClick);
 }
-
-mymap.setView(latlng, 13);
-if (marker) {
-    marker.setLatLng(latlng);
-} else {
-    marker = L.marker(latlng).addTo(mymap);
-}
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-        '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-    tileSize: 512,
-    zoomOffset: -1,
-}).addTo(mymap);
 
 function onMapClick(e) {
     if (marker) {
@@ -52,5 +64,4 @@ function onMapClick(e) {
     latitudeInput.value = e.latlng.lat;
     longitudeInput.value = e.latlng.lng;
 }
-
-mymap.on("click", onMapClick);
+//#endregion
--- public/js/auth/register.js
+++ public/js/auth/register.js
@@ -1,75 +1,90 @@
+//#region déclarations
 const roleRadios = document.querySelectorAll('input[name="role"]');
 const heroContainer = document.querySelector("#hero-container");
-
-window.addEventListener("DOMContentLoaded", (event) => {
-    heroContainer.style.display = "none";
-});
-
-roleRadios.forEach((radio) => {
-    radio.checked = false;
-    radio.addEventListener("change", (event) => {
-        if (event.target.value === "hero") {
-            heroContainer.style.display = "block";
-        } else {
-            heroContainer.style.display = "none";
-        }
-    });
-});
 
 var mymap = L.map("map").setView([0, 0], 13);
 var latitudeInput = document.getElementById("latitude");
 var longitudeInput = document.getElementById("longitude");
 var marker;
 var latlng;
+//#endregion
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-            latlng = L.latLng(
-                position.coords.latitude,
-                position.coords.longitude
-            );
+//#region événements
+window.addEventListener("DOMContentLoaded", (event) => {
+    heroContainer.style.display = "none";
 
-            mymap.setView(latlng, 13);
-            if (marker) {
-                marker.setLatLng(latlng);
+    setLocation();
+    initRoleRadios();
+    initMap();
+});
+
+function initRoleRadios() {
+    roleRadios.forEach((radio) => {
+        radio.checked = false;
+        radio.addEventListener("change", (event) => {
+            if (event.target.value === "hero") {
+                heroContainer.style.display = "block";
             } else {
-                marker = L.marker(latlng).addTo(mymap);
+                heroContainer.style.display = "none";
             }
-
-            latitudeInput.value = position.coords.latitude;
-            longitudeInput.value = position.coords.longitude;
-        },
-        function (error) {
-            console.error(error);
-        }
-    );
-} else {
-    console.error("Geolocation is not supported by this browser.");
+        });
+    });
 }
 
-if (latlng == null) {
-    var lat = 49.3823959;
-    var lng = 1.0751564;
-    latlng = L.latLng(lat, lng);
-    latitudeInput.value = lat;
-    longitudeInput.value = lng;
+function setLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                latlng = L.latLng(
+                    position.coords.latitude,
+                    position.coords.longitude
+                );
+
+                mymap.setView(latlng, 13);
+                if (marker) {
+                    marker.setLatLng(latlng);
+                } else {
+                    marker = L.marker(latlng).addTo(mymap);
+                }
+
+                latitudeInput.value = position.coords.latitude;
+                longitudeInput.value = position.coords.longitude;
+            },
+            function (error) {
+                console.error(error);
+            }
+        );
+    } else {
+        console.error("Geolocation is not supported by this browser.");
+    }
+
+    if (latlng == null) {
+        var lat = 48.8566806;
+        var lng = 2.3298617;
+        latlng = L.latLng(lat, lng);
+        latitudeInput.value = lat;
+        longitudeInput.value = lng;
+    }
 }
 
-mymap.setView(latlng, 13);
-if (marker) {
-    marker.setLatLng(latlng);
-} else {
-    marker = L.marker(latlng).addTo(mymap);
-}
+function initMap() {
+    mymap.setView(latlng, 13);
+    if (marker) {
+        marker.setLatLng(latlng);
+    } else {
+        marker = L.marker(latlng).addTo(mymap);
+    }
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-        '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-    tileSize: 512,
-    zoomOffset: -1,
-}).addTo(mymap);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+            '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+        tileSize: 512,
+        zoomOffset: -1,
+    }).addTo(mymap);
+
+    mymap.on("click", onMapClick);
+}
 
 function onMapClick(e) {
     if (marker) {
@@ -80,5 +95,4 @@ function onMapClick(e) {
     latitudeInput.value = e.latlng.lat;
     longitudeInput.value = e.latlng.lng;
 }
-
-mymap.on("click", onMapClick);
+//#endregion
